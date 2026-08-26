4bc14aa3f15110b15340985668dfe8b2bf92d9dd
poonamm05 <poonampatidar0755@gmail.com>
2026-08-26 22:24:21 +0530

    code

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 
 // API Routes
